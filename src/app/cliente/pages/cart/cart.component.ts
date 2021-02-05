4430b648dec6580f0bfe3c8bf3586db731f84342
Franco <francomorales145@gmail.com>
2021-02-05 16:12:36 -0300

    Página provisoria del carrito lista

--- src/app/cliente/pages/cart/cart.component.ts
+++ src/app/cliente/pages/cart/cart.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
+  public items:object[] = [
+    {
+      name:"food 01",
+      img: "https://place-hold.it/500x300",
+      price:100,
+    },
+    {
+      name:"food 02",
+      img: "https://place-hold.it/500x300",
+      price:250
+    },
+    {
+      name:"food 03",
+      img: "https://place-hold.it/500x300",
+      price:300
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
